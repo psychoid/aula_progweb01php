3860427625ffb5d93db244ec15f13371440005c2
psychoid <msr112@gmail.com>
2020-09-18 22:51:43 -0300

    primeiro commit

---
+++ cadpessoa.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nome   = $_POST['nome'];
+    $idade  = $_POST['idade'];
+    $cpf    = $_POST['cpf'];
+
+    
+    echo $nome . "<br/>";
+    echo $idade . "<br/>";
+    echo $cpf . "<br/>";
+
+
+
+?>
